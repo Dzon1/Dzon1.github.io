f296cace86d16ae9eb869b73bbc103016e1ad22e
Dzon1 <27859980+Dzon1@users.noreply.github.com>
2019-06-08 19:39:23 +0200

    Update process.js

--- process.js
+++ process.js
@@ -1,7 +1,7 @@
 var levo = ["ld klub","eve","rotmans siroki","rotmans slim","ld club"];
 var iza = ["rotmans","ld","monus","cesterfild","de santis"];
 var ukoso_desno = ["bond","winston","monte carlo","viceroy","fast","gauloises","next","davidoff","karelija"];
-var desno = ["lm","lucky strike","laki strajk","west","mallboro","camel","jade","diva","pall mall","ronhill","opposite","parlament"];
+var desno = ["lm","lucky strike","laki strajk","west","marlboro","camel","jade","diva","pall mall","ronhill","opposite","parlament"];
 
 console.log('test');
 var msg;
@@ -14,19 +14,19 @@ forma.addEventListener("submit",function loadDoc(e) {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-    
+
   if (article !== '') {
 	msg='Trazeni artikl <b style="font-size:2rem">"'+article+'"</b> ';
 	if (levo.includes(article.toLowerCase())) {
-		msg+="je <span style='color:red;font-size:2rem;'>levo</span>";
+		msg+="je <span style='color:red;font-size:2rem;'>levo</span><img height='50px' width='50px' src='https://cdn.pixabay.com/photo/2015/08/19/16/14/arrow-896214_640.png'/>";
 	}else if(iza.includes(article.toLowerCase())){
-		msg+="je <span style='color:red;font-size:2rem;'>iza</span>";
+		msg+="je <span style='color:red;font-size:2rem;'>iza</span><img height='50px' width='50px' src='https://cdn2.iconfinder.com/data/icons/arrows-vol-1-1/32/down1-512.png'/>";
 	}else if(ukoso_desno.includes(article.toLowerCase())){
-		msg+="je <span style='color:red;font-size:2rem;'>ukoso-desno</span>";
+		msg+="je <span style='color:red;font-size:2rem;'>ukoso-desno</span><img height='50px' width='80px' src='https://www.charbase.com/images/glyph/8600'/>"
 	}else if(desno.includes(article.toLowerCase())){
-        msg+="je <span style='color:red;font-size:2rem;'>desno</span>";
+        msg+="je <span style='color:red;font-size:2rem;'>desno</span><img height='50px' width='80px' src='https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Arrow_east.svg/800px-Arrow_east.svg.png'/>";
 	} else{
-		msg+= "<span style='color:red;font-size:2rem;'>nije na stanju</span>";
+		msg+= "<span style='color:red;font-size:2rem;'>nije na stanju</span><img height='50px' width='50px' src='https://www.charbase.com/images/glyph/8600'/>";
 	}
 }else{
 	msg= "<span style='color:red'>Niste uneli tekst</span>";
