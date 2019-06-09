4a636987a148da1ca556be1f79d204203157ab05
Dzon1 <27859980+Dzon1@users.noreply.github.com>
2019-06-09 20:52:29 +0200

    Update process.js

--- process.js
+++ process.js
@@ -2,8 +2,6 @@ var levo = ["ld klub","eve","rotmans siroki","rotmans slim","ld club"];
 var iza = ["rotmans","ld","monus","cesterfild","de santis"];
 var ukoso_desno = ["bond","winston","monte carlo","viceroy","fast","gauloises","next","davidoff","karelija"];
 var desno = ["lm","lucky strike","laki strajk","west","marlboro","camel","jade","diva","pall mall","ronhill","opposite","parlament"];
-alert("Zdravo Dzoni");
-console.log('test');
 var msg;
 var forma = document.getElementById('forma');
 var result = document.getElementById('result');
