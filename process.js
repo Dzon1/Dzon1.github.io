3b72df03c74b76660c3399a1f704b3e6e176b67d
Dzon1 <27859980+Dzon1@users.noreply.github.com>
2020-08-01 09:39:39 +0200

    Update process.js

--- process.js
+++ process.js
@@ -10,7 +10,7 @@ var  arrow= document.getElementById('arrow');
 var ac=$("#arrow_container");
 forma.addEventListener("submit",function loadDoc(e) {
 	e.preventDefault();
-        ac.fadeIn(slow);
+        ac.fadeIn("slow");
 	msg='';
 	var article = document.getElementById('article').value.trim();
   var xhttp = new XMLHttpRequest();
