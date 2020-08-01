eda6b2ea52ed378ca94559dc2002091984347642
Dzon1 <27859980+Dzon1@users.noreply.github.com>
2020-08-01 09:26:50 +0200

    Update process.js

--- process.js
+++ process.js
@@ -1,3 +1,4 @@
+$(document).ready(function(){
 var levo = ["ld klub","eve","rotmans siroki","rotmans slim","ld club"];
 var iza = ["rotmans","ld","monus","cesterfild","de santis"];
 var ukoso_desno = ["bond","winston","monte carlo","viceroy","fast","gauloises","next","davidoff","karelija"];
@@ -44,6 +45,7 @@ forma.addEventListener("submit",function loadDoc(e) {
   xhttp.open("GET", "", true);
   xhttp.send();
 });
+}
 
 
 
